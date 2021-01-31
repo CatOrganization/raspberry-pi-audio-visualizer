666f3546e02877aaaab2665dc94f255e8454601f
Michael Moghaddam <6424916+mmoghaddam385@users.noreply.github.com>
2021-01-31 13:27:50 -0800

    rename freq and wave vis, clean up make file

--- c_version/src/main.c
+++ c_version/src/main.c
@@ -138,7 +138,7 @@ int main(int argc, char *argv[])
         NewSoundWaveVis(),
         NewDvdLogoVis(),
         NewTimeDomainVis(),
-        NewFrequencyDomainVis()
+        NewFrequencyAndWaveVis()
     };
 
     fprintf(stdout, "initializing visualizations\n");
