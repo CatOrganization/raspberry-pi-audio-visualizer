d5788fa502ee5467760767b8248ba01fb004d17b
Michael Moghaddam <6424916+mmoghaddam385@users.noreply.github.com>
2022-01-12 23:19:57 -0500

    triple line vis WIP

--- src/filter.c
+++ src/filter.c
@@ -2,9 +2,9 @@
 
 // Low pass bass filter that filters out frequencies > 500Hz
 // Coefficients calculated via SciPy: scipy.signal.butter(4, 500, bytpe='lowpass', fs=44100)
-double lowPassBassFilterNumeratorCoefficients[] = {1.46901845e-6, 5.87607379e-6, 8.81411069e-6, 5.87607379e-6, 1.46901845e-6}; 
-double lowPassBassFilterDenominatorCoefficients[] = {-3.81386538, 5.45872379, -3.47494261, 0.83010771}; 
-const LinearFilter LowPassBassFilter = { 
+double lowPassBassFilterNumeratorCoefficients[] = {1.46901845e-6, 5.87607379e-6, 8.81411069e-6, 5.87607379e-6, 1.46901845e-6};
+double lowPassBassFilterDenominatorCoefficients[] = {-3.81386538, 5.45872379, -3.47494261, 0.83010771};
+const LinearFilter LowPassBassFilter = {
         4, // 4th order linear filter
         lowPassBassFilterNumeratorCoefficients,
         lowPassBassFilterDenominatorCoefficients
@@ -31,20 +31,20 @@ double apply_linear_filter(LinearFilter filter, double *input, double **output, 
     double input_influence = 0;
     double output_influence = 0;
     double max = 0;
-        
+
     // We have to start at i = filter.order so we have enough look back buffer
     for (int i = filter.order; i < size; i++)
     {
         input_influence = 0;
         output_influence = 0;
-        
+
         // Calculate the input_influence value
         for (int j = 0; j <= filter.order; j++)
         {
             input_influence += filter.b[j] * input[i - j];
         }
 
-        // Calculatr the output_influence value
+        // Calculate the output_influence value
         for (int j = 0; j < filter.order; j++)
         {
             output_influence += filter.a[j] * (*output)[i - j - 1];
